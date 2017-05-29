86fa1d50038dbb4a85b2676dd200f3f7e7260afb
Mohit21k <Katyal.mohit@gmail.com>
2017-05-29 18:28:37 +0530

    Add files via upload

--- src/main/java/com/db/demo/services/ShopInfoService.java
+++ src/main/java/com/db/demo/services/ShopInfoService.java
@@ -7,10 +7,43 @@ import com.db.demo.model.ShopInfo;
 
 
 
+
+/**
+ * The Interface ShopInfoService.
+ */
 public interface ShopInfoService {
 
+	/**
+	 * Gets the shop details.
+	 *
+	 * @return the shop details
+	 */
 	public List<ShopInfo> getShopDetails();
+	
+	/**
+	 * Gets the shop info.
+	 *
+	 * @param id the id
+	 * @return the shop info
+	 */
 	public ShopInfo getShopInfo(Integer id);
-	public void addShopDetails(ShopInfo shopInfo) throws IOException;
+	
+	/**
+	 * Adds the shop details.
+	 *
+	 * @param shopInfo the shop info
+	 * @return the shop info
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	public ShopInfo addShopDetails(ShopInfo shopInfo) throws IOException;
+	
+	/**
+	 * Gets the shop details.
+	 *
+	 * @param lat the lat
+	 * @param lng the lng
+	 * @return the shop details
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
 	public ShopInfo getShopDetails(String lat,String lng) throws IOException;
 }
