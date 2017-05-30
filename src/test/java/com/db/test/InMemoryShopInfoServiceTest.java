3ee5ed85cede4a26def7bb4a59ee9de4887704d2
Mohit21k <Katyal.mohit@gmail.com>
2017-05-30 12:51:41 +0530

    Add files via upload

--- src/test/java/com/db/test/InMemoryShopInfoServiceTest.java
+++ src/test/java/com/db/test/InMemoryShopInfoServiceTest.java
@@ -2,13 +2,12 @@ package com.db.test;
 
 import java.io.IOException;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
-
 import com.db.demo.model.ShopInfo;
 import com.db.web.services.impl.InMemoryShopInfoServiceImpl;
-
 
 // TODO: Auto-generated Javadoc
 /**
@@ -16,41 +15,63 @@ import com.db.web.services.impl.InMemoryShopInfoServiceImpl;
  */
 public class InMemoryShopInfoServiceTest {
 
-	
+	/** The Constant logger. */
+	private static final Logger logger = LoggerFactory.getLogger(InMemoryShopInfoServiceTest.class);
+
 	/**
 	 * Adds the shop details Test Case.
-	 *
-	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
 	@Test
-	public void addShopDetailsTC() throws IOException{
-		InMemoryShopInfoServiceImpl infoServiceImpl = new InMemoryShopInfoServiceImpl();
-		ShopInfo shopInfo = new ShopInfo();
-		shopInfo.setShopName("Domino's");
-		shopInfo.setShopAddress("MBP Mahape");
-		shopInfo.setShopPostCode("400710");
-		infoServiceImpl.addShopDetails(shopInfo);
-		AssertJUnit.assertEquals("1", shopInfo.getShopId());
+	public void addShopDetailsTC() {
+		try {
+			InMemoryShopInfoServiceImpl infoServiceImpl = new InMemoryShopInfoServiceImpl();
+			ShopInfo shopInfo = new ShopInfo();
+			shopInfo.setShopName("Domino's");
+			shopInfo.setShopAddress("MBP Mahape");
+			shopInfo.setShopPostCode("400710");
+			infoServiceImpl.addShopDetails(shopInfo);
+			AssertJUnit.assertEquals("1", shopInfo.getShopId());
+		} catch (Exception e) {
+			logger.error("Error Message =" + e.getMessage());
+		}
+
 	}
-	
-	
+
+	/**
+	 * Adds the shop details null value.
+	 */
+	@Test
+	public void addShopDetailsNullValue() {
+		try {
+			InMemoryShopInfoServiceImpl infoServiceImpl = new InMemoryShopInfoServiceImpl();
+			ShopInfo shopInfo = null;
+			infoServiceImpl.addShopDetails(shopInfo);
+			AssertJUnit.assertNull(shopInfo);
+		} catch (Exception e) {
+			logger.error("Error Message =" + e.getMessage());
+		}
+	}
+
 	/**
 	 * Gets the shop details Test Case.
 	 *
 	 * @return the shop details TC
-	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
 	@Test
-	public void getShopDetailsTC() throws IOException{
+	public void getShopDetailsTC() {
 		InMemoryShopInfoServiceImpl infoServiceImpl = new InMemoryShopInfoServiceImpl();
 		ShopInfo shopInfo = new ShopInfo();
 		shopInfo.setShopName("Domino's");
 		shopInfo.setShopAddress("MBP Mahape");
 		shopInfo.setShopPostCode("400710");
-		infoServiceImpl.addShopDetails(shopInfo);
-		shopInfo = infoServiceImpl.getShopDetails("19.1585", "72.9989");
-		AssertJUnit.assertEquals("Domino's", shopInfo.getShopName());
-		AssertJUnit.assertEquals("MBP Mahape", shopInfo.getShopAddress());
-		AssertJUnit.assertEquals("400710", shopInfo.getShopPostCode());
+		try {
+			infoServiceImpl.addShopDetails(shopInfo);
+			shopInfo = infoServiceImpl.getShopDetails("19.1585", "72.9989");
+			AssertJUnit.assertEquals("Domino's", shopInfo.getShopName());
+			AssertJUnit.assertEquals("MBP Mahape", shopInfo.getShopAddress());
+			AssertJUnit.assertEquals("400710", shopInfo.getShopPostCode());
+		} catch (Exception e) {
+			logger.error("Error Message =" + e.getMessage());
+		}
 	}
 }
--- src/test/java/com/db/test/SpringBootUtilsTest.java
+++ src/test/java/com/db/test/SpringBootUtilsTest.java
@@ -1,16 +1,20 @@
 package com.db.test;
 
 import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 import com.db.demo.model.GoogleResponseToJson;
 import com.db.utils.SpringBootUtils;
 
-
 /**
  * The Class SpringBootUtilsTest.
  */
 public class SpringBootUtilsTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(SpringBootUtilsTest.class);
 
 	/**
 	 * Convert to lat long.
@@ -19,11 +23,16 @@ public class SpringBootUtilsTest {
 	 *             Signals that an I/O exception has occurred.
 	 */
 	@Test
-	public void convertToLatLong() throws IOException {
-		GoogleResponseToJson response = SpringBootUtils.convertToLatLong("MBP Mahape");
+	public void convertToLatLong() {
+		try {
+			GoogleResponseToJson response = SpringBootUtils.convertToLatLong("MBP Mahape");
 
-		AssertJUnit.assertEquals("19.1087863", (response.getResults())[0].getGeometry().getLocation().getLat());
-		AssertJUnit.assertEquals("73.0201256", (response.getResults())[0].getGeometry().getLocation().getLng());
+			AssertJUnit.assertEquals("19.1087863", (response.getResults())[0].getGeometry().getLocation().getLat());
+			AssertJUnit.assertEquals("73.0201256", (response.getResults())[0].getGeometry().getLocation().getLng());
+		} catch (Exception e) {
+			logger.error("Error Message =" + e.getMessage());
+		}
+
 	}
 
 	/**
@@ -31,6 +40,7 @@ public class SpringBootUtilsTest {
 	 */
 	@Test
 	public void calDistance() {
-		//AssertJUnit.assertEquals("5.960686764301944", SpringBootUtils.calDistance(19.1585, 72.9989, 19.1087863, 73.0201256));
+		AssertJUnit.assertEquals(5.960686764301944,
+				SpringBootUtils.calDistance(19.1585, 72.9989, 19.1087863, 73.0201256));
 	}
 }
